e595f5ecb52ed02d312956332940ceb486702970
RUDRANSH AGNIHOTRI <59211507+rudransh2004@users.noreply.github.com>
2020-05-12 16:23:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+
+from flask import Flask, render_template, Response
+
+import cv2
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+ds_factor=0.6
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        success, image = self.video.read()
+        image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
+        gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        face_rects=face_cascade.detectMultiScale(gray,1.3,5)
+        for (x,y,w,h) in face_rects:
+        	cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        	break
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False)
